e550274dd8d177bbb68e60fe9b6e0376899fa937
muddassiralirana <muddassiralirana786@gmail.com>
2022-12-28 18:01:11 +0500

    add api in mysql

--- mysqlApi.js
+++ mysqlApi.js
@@ -15,9 +15,29 @@ app.get("/" ,(req,res)=>{
             res.send(result)
         }
     })
+})
 
+app.post("/" , (req,res)=>{
+    con.query("INSERT INTO users SET ?",req.body,(err, result, field)=>{
+        if(err) err;
+        res.send(result)
+    })
+})
 
-    
+app.put("/:id" , (req, res)=>{
+    let data= [req.body.name, req.params.id];
+    con.query("UPDATE users SET name= ? where id = ? "  , data , (err,result,field) =>{
+        if(err) err;
+        res.send(result)
+    })
+})
+
+app.delete("/:id" , (req,res)=>{
+    let data = [req.params.id];
+    con.query("DELETE from users where id = "+data  , (err, result ,field)=>{
+        if(err) err;
+        res.send(result)
+    })
 })
 
 app.listen(5000,()=>{
